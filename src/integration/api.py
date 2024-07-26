06e5cda6f5c1ddfb8b266779d3e85f09d4ac68ee
henockk <henockgebriel@gmail.com>
2024-07-26 19:55:36 +0300

    integration

--- src/integration/api.py
+++ src/integration/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from retriever.retriever import create_retriever
-from generator.generator import generate_queries_decomposition, generate_answers
+from generator.generator import generate_queries_decomposition, generate_answers, create_query_generator, create_final_rag_chain
 
 app = Flask(__name__)
 
@@ -23,3 +23,20 @@ def generate_answers_api():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+from retriever import create_retriever
+from generator import create_query_generator, create_final_rag_chain
+
+def get_final_answer(question, doc_path):
+    retriever = create_retriever(doc_path)
+    query_generator = create_query_generator()
+    final_rag_chain = create_final_rag_chain(retriever)
+
+    retrieval_chain = query_generator | retriever.map()
+    docs = retrieval_chain.invoke({"question": question})
+
+    final_response_chain = final_rag_chain
+    response = final_response_chain.invoke({"question": question})
+    return response
+
